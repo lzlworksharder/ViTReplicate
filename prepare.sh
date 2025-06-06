89dbf1357f9f8892730386d7dd0bb7842d5caf6e
lzlworksharder <lzlworksharder@163.com>
2025-06-06 18:00:22 +0800

    preparation modified

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
 export HF_ENDPOINT=https://hf-mirror.com
+pip install -r requirements.txt
 huggingface-cli download uoft-cs/cifar100 --repo-type dataset --local-dir ./data/cifar100
 huggingface-cli download google/vit-large-patch16-224 --local-dir ./pretrained/vit-l-16
